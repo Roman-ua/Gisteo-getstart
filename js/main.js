cc47df3edfc2ba14b725a92e3cfefd6dceb8a77b
Roman-ua <60047390+Roman-ua@users.noreply.github.com>
2020-06-08 23:02:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,111 @@
+$(function(){
+
+    $(".header__nav-gamburger").click(function() {
+        $(".header__nav-items").toggleClass("active__menu"),
+        $("body").toggleClass("active__body");         
+        $(this).toggleClass("active");
+    });
+
+    $(".first, span").click(function() {
+        $(".first").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    $(".two").click(function() {
+        $(".two").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    $(".three").click(function() {
+        $(".three").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    $(".four").click(function() {
+        $(".four").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    $(".five").click(function() {
+        $(".five").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    $(".six").click(function() {
+        $(".six").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    $(".seven").click(function() {
+        $(".seven").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    $(".eight").click(function() {
+        $(".eight").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+
+    $(".nine").click(function() {
+        $(".nine").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    $(".ten").click(function() {
+        $(".ten").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".eleven").click(function() {
+        $(".eleven").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".twelve").click(function() {
+        $(".twelve").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".thirteen").click(function() {
+        $(".thirteen").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".fifteen").click(function() {
+        $(".fifteen").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".sexteen").click(function() {
+        $(".sexteen").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".seventeen").click(function() {
+        $(".seventeen").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".eighteen").click(function() {
+        $(".eighteen").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".nineteen").click(function() {
+        $(".nineteen").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".tventy").click(function() {
+        $(".tventy").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".tventy-one").click(function() {
+        $(".tventy-one").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+    $(".tventy-two").click(function() {
+        $(".tventy-two").toggleClass("active");       
+        // $(this).toggleClass("active");
+    });
+
+    
+    $(".form__button").click(function() {
+        $(".form__thank").toggleClass("active__text"),        
+        $(this).toggleClass("active__text");
+    });
+    
+    
+});
